3b6eb582b1c71fecb203073e6d658613df7aa808
tacokitan <80319979+tacokitan@users.noreply.github.com>
2023-02-22 22:39:30 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Home!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo.png" />
       </Head>
 
       <main>
